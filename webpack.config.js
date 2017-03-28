c33470937bbd2a75e957d1f613290b26255ab188
liubinyi <bliu1@berkeley.edu>
2017-03-27 20:24:45 -0700

    solve webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -31,16 +31,11 @@ var config = {
         loader: 'babel',
         exclude: /(node_modules|bower_components)/
       },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
     ]
   },
   resolve: {
     root: path.resolve('./src'),
-    extensions: ['', '.js']
+    extensions: ['.js']
   },
   plugins: plugins
 };
